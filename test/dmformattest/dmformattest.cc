3f96af0e61e715527252a3538e4c7abe37fd9a0f
brinkqiang <brink.qiang@gmail.com>
2020-05-15 17:21:00 +0800

    add

--- test/dmformattest/dmformattest.cc
+++ test/dmformattest/dmformattest.cc
@@ -12,15 +12,6 @@ int main()
     std::cout << fmt::format("{2},{1},{0}", "a", "b", "c") << std::endl;
     std::cout << fmt::format("{:%Y-%m-%d %H:%M:%S}", fmt::localtime(time(NULL))) << std::endl;
 
-    std::cout << fmt::lexical_cast<float>(fmt::to_string(std::numeric_limits<float>::max())) << std::endl;
-    std::cout << fmt::lexical_cast<double>(fmt::to_string(std::numeric_limits<double>::max())) << std::endl;
-    std::cout << fmt::lexical_cast<long double>(fmt::to_string(std::numeric_limits<long double>::max())) << std::endl;
-    std::cout << fmt::lexical_cast<int>(fmt::to_string(std::numeric_limits<int>::max())) << std::endl;
-    std::cout << fmt::lexical_cast<int>(fmt::to_string(std::numeric_limits<int>::min())) << std::endl;
-
-    std::cout << fmt::lexical_cast<int64_t>(fmt::to_string(std::numeric_limits<int64_t>::max())) << std::endl;
-    std::cout << fmt::lexical_cast<uint64_t>(fmt::to_string(std::numeric_limits<uint64_t>::max())) << std::endl;
-
     std::cout << fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}",
         1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd', 'e', 'f') << std::endl;
 
