f6db3df60481492f0c321381c92223916edccb86
brinkqiang <brink.qiang@gmail.com>
2020-12-12 23:31:12 +0800

    add

--- test/dmformattest/dmformattest.cc
+++ test/dmformattest/dmformattest.cc
@@ -1,22 +1,37 @@
 
 #include "dmformat.h"
-#include "dmtime.h"
+#include "dmranges.h"
 #include <iostream>
 #include <vector>
 #include <list>
 #include <sstream>
+#include "dmchrono.h"
 
 int main()
 {
-    std::cout << fmt::format("{0},{1},{2}", "a", "b", "c") << std::endl;
-    std::cout << fmt::format("{2},{1},{0}", "a", "b", "c") << std::endl;
-    std::cout << fmt::format("{:%Y-%m-%d %H:%M:%S}", fmt::localtime(time(NULL))) << std::endl;
+	fmt::print(fmt::format("{0},{1},{2}\n", "a", "b", "c"));
+	fmt::print(fmt::format("{2},{1},{0}\n", "a", "b", "c"));
 
-    std::cout << fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}",
-        1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd', 'e', 'f') << std::endl;
+	std::vector<std::vector<int32_t>> ivv{ {1, 2}, {3, 5}, {7, 11} };
+	auto ivf = fmt::format("{}\n", ivv);
 
-    std::cout << fmt::format("{_1}/{a_}/{A_}", fmt::arg("a_", 'a'),
-        fmt::arg("A_", "A"), fmt::arg("_1", 1)) << std::endl;
+	std::chrono::system_clock::time_point point = std::chrono::system_clock::now();
+
+	fmt::print(fmt::format("It is {:%Y-%m-%d %H:%M:%S}\n", point));
+
+	fmt::dynamic_format_arg_store<fmt::format_context> store;
+	store.push_back(42);
+	store.push_back("abc1");
+	store.push_back(1.5f);
+	fmt::print(fmt::vformat("{} and {} and {}\n", store));
+
+	auto t1 = std::make_tuple('a', 1, 2.0f);
+	fmt::print(fmt::format("{}\n", t1));
+	fmt::print(fmt::format("({})\n", fmt::join(t1, ",")));
+
+	fmt::print(fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}\n", 1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd', 'e', 'f'));
+
+	fmt::print(fmt::format("{NAME}-{AGE}-{JOB}\n", fmt::arg("NAME", "TOM"), fmt::arg("AGE", 32), fmt::arg("JOB", "player")));
 
     return 0;
 }
